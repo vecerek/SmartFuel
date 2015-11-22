1040a90d85e85048659b072c8377dfbcad556a5b
Gabriel <xlehoc01@stud.fit.vutbr.cz>
2015-11-22 15:50:18 +0100

    adds notification

--- app/src/main/java/sk/codekitchen/smartfuel/ui/SmartFuelActivity.java
+++ app/src/main/java/sk/codekitchen/smartfuel/ui/SmartFuelActivity.java
@@ -1,13 +1,21 @@
 package sk.codekitchen.smartfuel.ui;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import sk.codekitchen.smartfuel.R;
 import sk.codekitchen.smartfuel.ui.fragments.CustomViewPager;
@@ -18,28 +26,41 @@ import sk.codekitchen.smartfuel.ui.fragments.FragmentSettings;
 import sk.codekitchen.smartfuel.ui.fragments.FragmentShop;
 import sk.codekitchen.smartfuel.ui.fragments.FragmentStatistics;
 import sk.codekitchen.smartfuel.ui.views.ExtraboldTextView;
+import sk.codekitchen.smartfuel.ui.views.SemiboldTextView;
 
 /**
  * @author Gabriel Lehocky
  */
-public class SmartFuelActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class SmartFuelActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    public final static int NOTIFICATION_RECORDING_ID = 0;
 
     private DrawerLayout drawer;
     private Toolbar toolbar;
     private NavigationView menu;
     private ExtraboldTextView title;
+    private ImageView button_rec;
+    private ImageView button_stop;
+    private ExtraboldTextView txt_rec;
+    private ExtraboldTextView txt_stop;
+
+    private boolean isRecording = false;
 
     private CustomViewPager viewPager;
-    FragmentRecorder fRecorder = new FragmentRecorder();
-    FragmentStatistics fStatistics = new FragmentStatistics();
-    FragmentShop fShop = new FragmentShop();
-    FragmentProfile fProfile = new FragmentProfile();
-    FragmentSettings fSettings = new FragmentSettings();
+    private FragmentRecorder fRecorder = new FragmentRecorder();
+    private FragmentStatistics fStatistics = new FragmentStatistics();
+    private FragmentShop fShop = new FragmentShop();
+    private FragmentProfile fProfile = new FragmentProfile();
+    private FragmentSettings fSettings = new FragmentSettings();
+
+    private NotificationManager notificationManager;
+    private PendingIntent stopRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart_fuel);
+
         initLayout();
 
     }
@@ -50,6 +71,14 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         title = (ExtraboldTextView) findViewById(R.id.toolbar_title);
+        button_rec = (ImageView) findViewById(R.id.toolbar_rec);
+        button_rec.setOnClickListener(this);
+        button_stop = (ImageView) findViewById(R.id.toolbar_stop);
+        button_stop.setOnClickListener(this);
+        txt_rec = (ExtraboldTextView) findViewById(R.id.toolbar_rec_txt);
+        txt_rec.setOnClickListener(this);
+        txt_stop = (ExtraboldTextView) findViewById(R.id.toolbar_stop_txt);
+        txt_stop.setOnClickListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -93,21 +122,91 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
 
         if (id == R.id.nav_recorder) {
             viewPager.setCurrentItem(0, false);
+            toolbarIcons(true);
         }
         else if (id == R.id.nav_stat) {
             viewPager.setCurrentItem(1, false);
+            toolbarIcons(false);
         }
         else if (id == R.id.nav_shop) {
             viewPager.setCurrentItem(2, false);
+            toolbarIcons(false);
         }
         else if (id == R.id.nav_profile) {
             viewPager.setCurrentItem(3, false);
+            toolbarIcons(false);
         }
         else if (id == R.id.nav_settings) {
             viewPager.setCurrentItem(4, false);
+            toolbarIcons(false);
         }
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toolbar_rec_txt:
+            case R.id.toolbar_rec:
+                isRecording = true;
+                createNotification();
+                toolbarIcons(true);
+                break;
+            case R.id.toolbar_stop_txt:
+            case R.id.toolbar_stop:
+                isRecording = false;
+                destroyNotification();
+                toolbarIcons(true);
+                break;
+        }
+    }
+
+    private void toolbarIcons(boolean show){
+        if(show){
+            if(isRecording){
+                button_rec.setVisibility(View.GONE);
+                button_stop.setVisibility(View.VISIBLE);
+                txt_rec.setVisibility(View.GONE);
+                txt_stop.setVisibility(View.VISIBLE);
+            }
+            else {
+                button_rec.setVisibility(View.VISIBLE);
+                button_stop.setVisibility(View.GONE);
+                txt_rec.setVisibility(View.VISIBLE);
+                txt_stop.setVisibility(View.GONE);
+            }
+        }
+        else {
+            button_rec.setVisibility(View.GONE);
+            button_stop.setVisibility(View.GONE);
+            txt_rec.setVisibility(View.GONE);
+            txt_stop.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * HELP:
+     *      http://developer.android.com/training/notify-user/expanded.html
+     *      http://developer.android.com/guide/topics/ui/notifiers/notifications.html
+     */
+
+    private void createNotification(){
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder newNotification =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.speed)
+                .setContentTitle(getString(R.string.notification_title))
+                .addAction(R.drawable.toolbar_stop, getString(R.string.toolbar_stop), stopRecorder)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+        notificationManager.notify(NOTIFICATION_RECORDING_ID, newNotification.build());
+    }
+
+    private void destroyNotification(){
+        notificationManager.cancel(NOTIFICATION_RECORDING_ID);
     }
 }
