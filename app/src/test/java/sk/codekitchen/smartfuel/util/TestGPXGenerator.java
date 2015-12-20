783e2b68fded96f0de7d5d17ba27c5a98319a4e2
Attila Večerek <veat24@gmail.com>
2015-12-20 22:06:20 +0100

    Implements test that checks, whether the GPXGenerator object has been created successfully.

--- app/src/test/java/sk/codekitchen/smartfuel/util/TestGPXGenerator.java
+++ app/src/test/java/sk/codekitchen/smartfuel/util/TestGPXGenerator.java
@@ -1,11 +1,16 @@
 package sk.codekitchen.smartfuel.util;
 
+import android.content.Context;
 import android.os.Build;
 import android.test.AndroidTestCase;
 
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import java.io.File;
@@ -20,10 +25,31 @@ import sk.codekitchen.smartfuel.BuildConfig;
 @RunWith(RobolectricGradleTestRunner.class)
 public class TestGPXGenerator extends AndroidTestCase {
 
+	protected Context context;
+	protected static final String TEST_FILE_WITH_SPEED = "res/gpx_with_speed.gpx";
+	protected static final String TEST_FILE_WITHOUT_SPEED = "res/gpx_without_speed.gpx";
+
+	@Before
+	public void setUp() {
+		this.context = RuntimeEnvironment.application.getApplicationContext();
+	}
+
 	@Test
 	public void fileObjectShouldNotBeNull() throws Exception {
 		File file = getFileFromPath(this, "res/gpx_with_speed.gpx");
 		assertNotNull("Error: file not loaded", file);
+	}
+
+	@Test
+	public void gpxGeneratorObjectShouldBeCreated() throws Exception {
+		try {
+			File gpxFile = getFileFromPath(this, TEST_FILE_WITH_SPEED);
+			GPXGenerator gpx = new GPXGenerator(this.context, gpxFile);
+			Assert.assertEquals(gpx.getNumLocations(), gpx.getNodesNumberOf("trkpt"));
+
+		} catch (Exception e) {
+			Assert.fail("Object construction shouldn't throw an exception: " + e.getMessage());
+		}
 	}
 
 	private static File getFileFromPath(Object obj, String fileName) {
