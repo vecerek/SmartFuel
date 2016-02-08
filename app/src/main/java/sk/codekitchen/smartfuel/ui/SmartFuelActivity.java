a94f47092b89ddaedd3e0eb992b1e5fa253b6802
Attila Večerek <veat24@gmail.com>
2016-02-08 21:11:42 +0100

    Implements marshmallow permission request

--- app/src/main/java/sk/codekitchen/smartfuel/ui/SmartFuelActivity.java
+++ app/src/main/java/sk/codekitchen/smartfuel/ui/SmartFuelActivity.java
@@ -1,12 +1,17 @@
 package sk.codekitchen.smartfuel.ui;
 
+import android.annotation.TargetApi;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.opengl.Visibility;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,7 +22,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import java.util.ArrayList;
+
 import sk.codekitchen.smartfuel.R;
+import sk.codekitchen.smartfuel.service.GPSTrackerService;
 import sk.codekitchen.smartfuel.ui.fragments.CustomViewPager;
 import sk.codekitchen.smartfuel.ui.fragments.FragmentAdapter;
 import sk.codekitchen.smartfuel.ui.fragments.FragmentProfile;
@@ -34,6 +44,15 @@ import sk.codekitchen.smartfuel.ui.views.SemiboldTextView;
 public class SmartFuelActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     public final static int NOTIFICATION_RECORDING_ID = 0;
+    private final static int FINE_LOCATION_RESULT = 100;
+
+    /**
+     * Android Marshmallow Permission Request instance variables.
+     */
+    private SharedPreferences sharedPreferences;
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected;
+    private View coordinatorLayoutView;
 
     private DrawerLayout drawer;
     private Toolbar toolbar;
@@ -62,6 +81,10 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
         setContentView(R.layout.activity_smart_fuel);
 
         initLayout();
+
+        //for determining if we have asked the questions..
+        sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+        coordinatorLayoutView = findViewById(R.id.drawer_layout);
 
     }
 
@@ -146,40 +169,48 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
         return true;
     }
 
+    @TargetApi(23)
     @Override
     public void onClick(View v) {
+        ArrayList<String> permissions = new ArrayList<>();
+        int resultCode = 0;
+
         switch (v.getId()){
             case R.id.toolbar_rec_txt:
             case R.id.toolbar_rec:
-                isRecording = true;
-                createNotification();
-                toolbarIcons(true);
+                permissions.add(ACCESS_FINE_LOCATION);
+                resultCode = FINE_LOCATION_RESULT;
                 break;
             case R.id.toolbar_stop_txt:
             case R.id.toolbar_stop:
                 isRecording = false;
                 destroyNotification();
                 toolbarIcons(true);
+                //stopService(new Intent(this, GPSTrackerService.class));
                 break;
+        }
+
+        if (resultCode != 0) {
+            checkRecordingPermissions(permissions, resultCode);
         }
     }
 
-    private void toolbarIcons(boolean show){
-        if(show){
-            if(isRecording){
+    private void toolbarIcons(boolean show) {
+        if (show) {
+            if (isRecording) {
                 button_rec.setVisibility(View.GONE);
                 button_stop.setVisibility(View.VISIBLE);
                 txt_rec.setVisibility(View.GONE);
                 txt_stop.setVisibility(View.VISIBLE);
-            }
-            else {
+
+            } else {
                 button_rec.setVisibility(View.VISIBLE);
                 button_stop.setVisibility(View.GONE);
                 txt_rec.setVisibility(View.VISIBLE);
                 txt_stop.setVisibility(View.GONE);
             }
-        }
-        else {
+
+        } else {
             button_rec.setVisibility(View.GONE);
             button_stop.setVisibility(View.GONE);
             txt_rec.setVisibility(View.GONE);
@@ -193,7 +224,7 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
      *      http://developer.android.com/guide/topics/ui/notifiers/notifications.html
      */
 
-    private void createNotification(){
+    private void createNotification() {
 
         notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -207,7 +238,190 @@ public class SmartFuelActivity extends AppCompatActivity implements NavigationVi
         notificationManager.notify(NOTIFICATION_RECORDING_ID, newNotification.build());
     }
 
-    private void destroyNotification(){
+    private void startRecording() {
+        isRecording = true;
+        createNotification();
+        toolbarIcons(true);
+        //startService(new Intent(this, GPSTrackerService.class));
+    }
+
+    private void destroyNotification() {
         notificationManager.cancel(NOTIFICATION_RECORDING_ID);
+    }
+
+    @TargetApi(23)
+    private void checkRecordingPermissions(ArrayList<String> permissions, int resultCode) {
+        //filter out the permissions we have already accepted
+        permissionsToRequest = findUnAskedPermissions(permissions);
+        //get the permissions we have asked for before but are not granted..
+        //we will store this in a global list to access later.
+        permissionsRejected = findRejectedPermissions(permissions);
+
+        if (permissionsToRequest.size() > 0) { //we need to ask for permissions
+            //but have we already asked for them?
+            requestPermissions(permissionsToRequest
+                    .toArray(new String[permissionsToRequest.size()]), resultCode);
+            //mark all these as asked..
+            for(String perm : permissionsToRequest) {
+                markAsAsked(perm);
+            }
+
+        } else {
+            //show the success banner
+            if (permissionsRejected.size() < permissions.size()) {
+                //this means we can show success because some were already accepted.
+                startRecording();
+            }
+
+            if (permissionsRejected.size() > 0) {
+                //we have none to request but some previously rejected..tell the user.
+                //It may be better to show a dialog here in a prod application
+                Snackbar
+                        .make(coordinatorLayoutView, String.valueOf(permissionsRejected.size()) +
+                                " permission(s) were previously rejected", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Allow to Ask Again", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                for (String perm: permissionsRejected) {
+                                    clearMarkAsAsked(perm);
+                                }
+                            }
+                        })
+                        .show();
+            }
+        }
+    }
+
+    /**
+     * This is the method that is hit after the user accepts/declines the
+     * permission you requested. For the purpose of this example I am showing a "success" header
+     * when the user accepts the permission and a snackbar when the user declines it.  In your
+     * application you will want to handle the accept/decline in a way that makes sense.
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch (requestCode) {
+            case FINE_LOCATION_RESULT:
+                if (hasPermission(ACCESS_FINE_LOCATION)) {
+                    startRecording();
+                } else {
+                    permissionsRejected.add(ACCESS_FINE_LOCATION);
+                    makePostRequestSnack();
+                }
+                break;
+        }
+
+    }
+
+    /**
+     * a method that will centralize the showing of a snackbar
+     */
+    private void makePostRequestSnack() {
+        Snackbar
+                .make(coordinatorLayoutView, String.valueOf(permissionsRejected.size()) +
+                        " permission(s) were rejected", Snackbar.LENGTH_LONG)
+                .setAction("Allow to Ask Again", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        for (String perm: permissionsRejected) {
+                            clearMarkAsAsked(perm);
+                        }
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * method that will return whether the permission is accepted. By default it is true if the user
+     * is using a device below version 23
+     * @param permission
+     * @return
+     */
+    @TargetApi(23)
+    private boolean hasPermission(String permission) {
+        if (canMakeSmores()) {
+            return(checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
+        }
+        return true;
+    }
+
+    /**
+     * method to determine whether we have asked
+     * for this permission before.. if we have, we do not want to ask again.
+     * They either rejected us or later removed the permission.
+     * @param permission
+     * @return
+     */
+    private boolean shouldWeAsk(String permission) {
+        return(sharedPreferences.getBoolean(permission, true));
+    }
+
+    /**
+     * we will save that we have already asked the user
+     * @param permission
+     */
+    private void markAsAsked(String permission) {
+        sharedPreferences.edit().putBoolean(permission, false).apply();
+    }
+
+    /**
+     * We may want to ask the user again at their request.. Let's clear the
+     * marked as seen preference for that permission.
+     * @param permission
+     */
+    private void clearMarkAsAsked(String permission) {
+        sharedPreferences.edit().putBoolean(permission, true).apply();
+    }
+
+
+    /**
+     * This method is used to determine the permissions we do not have accepted yet and ones that we
+     * have not already bugged the user about.  This comes in handle when you are asking for multiple
+     * permissions at once.
+     * @param wanted
+     * @return
+     */
+    private ArrayList<String> findUnAskedPermissions(ArrayList<String> wanted) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm) && shouldWeAsk(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * this will return us all the permissions we have previously asked for but
+     * currently do not have permission to use. This may be because they declined us
+     * or later revoked our permission. This becomes useful when you want to tell the user
+     * what permissions they declined and why they cannot use a feature.
+     * @param wanted
+     * @return
+     */
+    private ArrayList<String> findRejectedPermissions(ArrayList<String> wanted) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wanted) {
+            if (!hasPermission(perm) && !shouldWeAsk(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Just a check to see if we have marshmallows (version 23)
+     * @return
+     */
+    private boolean canMakeSmores() {
+        return(Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
     }
 }
