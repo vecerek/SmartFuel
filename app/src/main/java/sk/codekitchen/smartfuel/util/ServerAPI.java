6a4b2ea4b7ad86a85e2d11d9388dee95a95edb4a
Attila Večerek <veat24@gmail.com>
2016-02-29 20:15:01 +0100

    Implements multipart http request sending

--- app/src/main/java/sk/codekitchen/smartfuel/util/ServerAPI.java
+++ app/src/main/java/sk/codekitchen/smartfuel/util/ServerAPI.java
@@ -7,6 +7,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.io.File;
 import java.io.IOException;
 
 import java.io.BufferedReader;
@@ -18,6 +19,12 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import sk.codekitchen.smartfuel.model.SFDB;
 
 /**
@@ -28,17 +35,22 @@ public class ServerAPI {
 
 	private static final String USER_AGENT = "SmartFuel/1.0";
 	private static final String SERVER = "http://smartfuel.walkoflife.sk/";
+    private static final MediaType MEDIA_TYPE_XML = MediaType.parse("text/xml");
+    private static final String FILE_PREFIX = "FILE_";
 
+    private String url;
 	public int responseCode;
 
 	private HttpURLConnection con;
+    private final OkHttpClient client = new OkHttpClient();
 
 	public ServerAPI(String action) throws IOException {
 		this(action, SERVER);
 	}
 
 	public ServerAPI(String action, String server) throws IOException {
-		String url = server + action;
+        if (action.charAt(0) == '/') action = action.substring(1);
+		url = server + action;
 		con = (HttpURLConnection) (new URL(url)).openConnection();
 
 		//add request header
@@ -96,5 +108,41 @@ public class ServerAPI {
 		} else {
 			throw new IOException("Response is not a JSON");
 		}
+	}
+
+    /**
+     * Sends a multipart request to the specified URL.
+     *
+     * @param params POST request-like params
+     * @param files array of files to send
+     * @throws IOException if host unreachable
+     *                     if response code not 200
+     */
+	public void sendMultipartRequest(Map<String, String> params, File[] files) throws IOException {
+
+        MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder();
+        requestBodyBuilder.setType(MultipartBody.FORM);
+
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            requestBodyBuilder.addFormDataPart(param.getKey(), param.getValue());
+        }
+
+        int fileCounter = 1;
+        for (File file : files) {
+            requestBodyBuilder.addFormDataPart(FILE_PREFIX + Integer.toString(fileCounter),
+                    file.getName(), RequestBody.create(MEDIA_TYPE_XML, file));
+            fileCounter++;
+        }
+
+        RequestBody requestBody = requestBodyBuilder.build();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(requestBody)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) throw new IOException("Unexpected response code " + response);
+
+        System.out.println(response.body().string());
 	}
 }
